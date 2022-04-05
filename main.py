d7c35b764f4e29bb458e61525c64f6f10aa51e0f
Parham Nooralishahi <parham.nooralishahi@gmail.com>
2022-04-05 03:44:31 -0400

    implement ms coco handler

--- main.py
+++ main.py
@@ -13,12 +13,13 @@ def main():
     #     'binarize' : True,
     #     'num_workers' : 10
     # }
-    
+
     # coco
     config = {
         'dir_in' : '/home/phm/GoogleDrive/Personal/Datasets/my-dataset/thermal-segmentor/Pipe_Inspection/pipe_inspection_group01_20201117/label_orig',
         'file_out' : '/home/phm/Documents/temp/phm.coco', 
         'handler' : 'coco',
+        'num_workers' : 10,
         'categories' : {
             'defect' : 1, 
             'surface_defect' : 2
--- phm/coco.py
+++ phm/coco.py
@@ -1,5 +1,7 @@
 
+import functools
 import json
+import logging
 import os
 from datetime import datetime
 from typing import Any, Callable, Dict, List, Union
@@ -8,6 +10,8 @@ import numpy as np
 from pycococreatortools import pycococreatortools
 
 from phm.gimp import translator
+
+from multiprocessing import Process, Manager, Pool, Value
 
 def generate_categories(anot_dir : str):
     catdir = [ item for item in os.listdir(anot_dir) if os.path.isdir(os.path.join(anot_dir, item)) ]
@@ -20,32 +24,135 @@ def generate_categories(anot_dir : str):
         })
     return cat_list
 
-# CATEGORIES = [
-#     {
-#         'id': 1,
-#         'name': 'square',
-#         'supercategory': 'shape',
-#     },
-#     {
-#         'id': 2,
-#         'name': 'circle',
-#         'supercategory': 'shape',
-#     },
-#     {
-#         'id': 3,
-#         'name': 'triangle',
-#         'supercategory': 'shape',
-#     },
-# ]
+def __coco_core_process(file, images, anotations, helper, image_id, segmentation_id, args):
+        filename = os.path.basename(file)
+        res = helper(file, **args)
+        cs = res.keys()
 
-# CATEGORIES = {
-#     'sqaure' : 1,
-#     'circle' : 2,
-#     'triangle' : 3
-# }
+        image = Image.fromarray(np.uint8(res['original']))
+        image_info = pycococreatortools.create_image_info(
+            image_id.value, filename, image.size)
+        if args['dataset_name'] is not None:
+            image_info['dataset_name'] = args['dataset_name']
+        images.append(image_info)
+
+        for k in res.keys():
+            if k in args['categories']:
+                class_id = args['categories'][k]
+                category_info = {'id': class_id}
+                binary_mask = np.asarray(Image.fromarray(res[k]).convert('1')).astype(np.uint8)
+                annotation_info = pycococreatortools.create_annotation_info(
+                    segmentation_id.value, image_id.value, {**category_info, 'is_crowd' : False}, binary_mask,
+                    image.size, tolerance=2)
+                if annotation_info is not None:
+                    anotations.append(annotation_info)
+
+                segmentation_id.value += 1
+
+        image_id.value += 1
 
 @translator('coco')
 def handler_coco__(
+    files : Union[str,List[str]], 
+    helper : Callable, 
+    categories : List[str],
+    file_out : str = None, 
+    num_workers : int = 1, **kwargs):
+
+    coco_output = None
+    if num_workers <= 1:
+        logging.info('Running MS COCO handler in single mode ...')
+        coco_output = handler_coco_single__(files, helper, categories, file_out, **kwargs)
+    else:
+        logging.info('Running MS COCO handler in parallel mode ...')
+        coco_output = handler_coco_parallel__(files, helper, categories, file_out, num_workers=num_workers, **kwargs)
+
+    with open(file_out, 'w') as fout:
+        json.dump(coco_output, fout, indent = 4)
+
+
+def handler_coco_parallel__(
+    files : Union[str,List[str]], 
+    helper : Callable, 
+    categories : List[str],
+    file_out : str = None,
+    dataset_name : str = None,
+    description : str = '...',
+    url : str = 'https://www.linkedin.com/in/parham-nooralishahi/',
+    version : str = '1.0.0',
+    year : int = 2022,
+    contributor : str = 'phm',
+    date_created : str = None,
+    licenses : List[Dict] = None,
+    num_workers : int = 1, 
+    **kwargs : Dict[str,Any]):
+
+    now_str = datetime.utcnow().isoformat(' ')
+
+    _info = {
+        "description": description,
+        "url": url,
+        "version": version,
+        "year": year,
+        "contributor": contributor,
+        "date_created": now_str if date_created is None else date_created
+    }
+
+    _licenses = [
+        {
+            "id": 1,
+            "name": "Attribution-NonCommercial-ShareAlike License",
+            "url": "http://creativecommons.org/licenses/by-nc-sa/2.0/"
+        }
+    ] if licenses is None else licenses
+
+    coco_output = {
+        "info": _info,
+        "licenses": _licenses,
+        "categories": categories,
+        "images": [],
+        "annotations": []
+    }
+
+    with Manager() as manager:
+        fargs = manager.dict()
+        images = manager.list()
+        anotations = manager.list()
+        image_id = manager.Value(int, 1)
+        segmentation_id = manager.Value(int, 1)
+
+        fargs.update({
+            'categories' : categories,
+            'dataset_name' : dataset_name,
+            'description' : description,
+            'url' : url, 
+            'version' : version,
+            'year' : year,
+            'contributor' : contributor,
+            'date_created' : date_created,
+            **kwargs
+        })
+
+        logging.info("Initialize the worker pool ...")
+        pool = Pool(processes=num_workers)
+        logging.info("Mapping the jobs to the workers ...")
+        
+        pool.map(functools.partial(__coco_core_process, 
+            images=images, 
+            anotations=anotations, 
+            helper=helper,
+            image_id=image_id,
+            segmentation_id=segmentation_id,
+            args=fargs), files)
+        pool.close()
+        pool.join()
+    
+        coco_output['images'].extend(images)
+        coco_output['annotations'].extend(anotations)
+
+    return coco_output
+
+def handler_coco_single__(
     files : Union[str,List[str]], 
     helper : Callable, 
     categories : List[str],
@@ -118,6 +225,6 @@ def handler_coco__(
 
         image_id = image_id + 1
 
-    with open(file_out, 'w') as fout:
-        json.dump(coco_output, fout, indent = 4)
+    return coco_output
+
 
